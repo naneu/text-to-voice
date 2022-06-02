b3aa93946b9e6ee111d38690337af29bba76f638
naneu <nancysororo@gmail.com>
2022-06-02 10:46:39 +0300

    Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <section className="section">
+        <div className="home">
+          <div className="text">
+            <h3>Text to speech software</h3>
+            <p>
+              An easy way to convert text to voice that’s fast and
+              straightforward
+            </p>
+          </div>
+
+          <div className="dropbox">
+            <p>Drag and drop your file here</p>
+            <p>Choose a file</p>
+          </div>
+          <div className="btn">
+            <button className="convert">convert</button>
+            <button className="download-btn">download</button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
